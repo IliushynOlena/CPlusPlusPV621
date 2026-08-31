573979f728ced529fab3908135524b2f596af893
Olena Iliushyn <lenailyshun@gmail.com>
2026-08-31 19:20:18 +0300

    Add project files.

---
+++ 01_IntroToCPlusPlus/Source.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main() 
+{
+	//  \n - new line
+	//  \t - tabulation
+	//  \\ - \some text\
+	//  \' - 'some text'
+	//  \" - "some text"
+
+	cout << "Hello world!\n";
+	cout << "\t\tHello world!\n";
+	cout << "\t\t\tHello world!\n";
+	cout << "\\Hello\nworld!\\\n";
+	cout << "\'Hello world!\'\n";
+	cout << "\"Hello world!\"\n";
+
+	return 0;
+}
